905a66bdeacb8f9da1b8d21342c75e06903ee032
luishernandez27 <luishernandez.es@academia.umb.edu.co>
2024-12-12 11:54:12 -0500

    Agregar ruta para la raíz

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-// Importar dependencias necesarias
 const express = require('express');
 const cors = require('cors');
 
@@ -7,15 +6,20 @@ const app = express();
 
 // Configurar middlewares
 app.use(cors()); // Permitir solicitudes desde el front-end
-app.use(express.json()); // solicitudes con formato JSON
+app.use(express.json()); // Solicitudes con formato JSON
 
-// Ruta de prueba
+// Ruta de prueba para /
+app.get('/', (req, res) => {
+    res.send('Bienvenido al backend del proyecto!');
+});
+
+// Ruta de prueba para /api/messages
 app.get('/api/messages', (req, res) => {
     res.json({ message: 'Hola desde el back-end UMB!' });
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3001; // Utiliza el puerto proporcionado por Vercel o el puerto 3001
+const PORT = process.env.PORT || 3001; // Puerto dinámico o 3001
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
